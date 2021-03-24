634526303d4153256e1e72c0fbe22cdb939f11f1
masaaki1915 <masaaki1915@gmail.com>
2021-03-24 18:58:58 +0900

    practice abc085 c

--- src/practice/abc085_c.py
+++ src/practice/abc085_c.py
@@ -6,35 +6,49 @@ C = -1
 
 y = Y // 1000
 
-# y = 10 * a + 5 * b + c
-
-if y == N:
-    A = 0
-    B = 0
-    C = N
-elif y == 10 * N:
-    A = N
-    B = 0
-    C = 0
-elif N < y and y < 10 * N:
-    for a in range(N)[::-1]:
-        if y <= 10 * a and y - (10 * a) > 5 * (N - a):
+if N <= y and y <= 10 * N:
+    for a in range(N + 1):
+        b = (y - N - 9 * a) / 4
+        if not b.is_integer() or b < 0 or b > N:
             continue
-        for b in range(N - a + 1)[::-1]:
-            if y - (10 * a) < 5 * b and y - (10 * a) - (5 * b) > (N - a - b):
-                continue
-            for c in range(N - a - b + 1)[::-1]:
-                if y - (10 * a) - (5 * b) != c or a + b + c != N:
-                    continue
-                A = a
-                B = b
-                C = c
-                break
-            else:
-                continue
-            break
-        else:
+        b = int(b)
+        c = N - a - b
+        if c < 0:
             continue
+        A = a
+        B = b
+        C = c
         break
 
 print(A, B, C)
+
+# y = 10 * a + 5 * b + c
+
+# if y == N:
+#     A = 0
+#     B = 0
+#     C = N
+# elif y == 10 * N:
+#     A = N
+#     B = 0
+#     C = 0
+# elif N < y and y < 10 * N:
+#     for a in range(N)[::-1]:
+#         if y <= 10 * a and y - (10 * a) > 5 * (N - a):
+#             continue
+#         for b in range(N - a + 1)[::-1]:
+#             if y - (10 * a) < 5 * b and y - (10 * a) - (5 * b) > (N - a - b):
+#                 continue
+#             for c in range(N - a - b + 1)[::-1]:
+#                 if y - (10 * a) - (5 * b) != c or a + b + c != N:
+#                     continue
+#                 A = a
+#                 B = b
+#                 C = c
+#                 break
+#             else:
+#                 continue
+#             break
+#         else:
+#             continue
+#         break
