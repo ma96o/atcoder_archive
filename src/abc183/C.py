5e723ce4c232b90c928a22ca4788e056bbc87c15
masaaki1915 <masaaki1915@gmail.com>
2021-04-02 23:55:01 +0900

    daily commit by cron

---
+++ src/abc183/C.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+
+for i in range(N):
+    [map(int, input().split())]
+
+
+N = 4
+a = []
+for i in range(2, N-1):
+    a[0] = 1
+    for j in range(1, N-1):
+        a[j] = i
