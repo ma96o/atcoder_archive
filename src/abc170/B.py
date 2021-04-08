e82929294dc5c8a38ba6fbcf1a5a35def0473792
masaaki1915 <masaaki1915@gmail.com>
2021-04-08 23:55:00 +0900

    daily commit by cron

---
+++ src/abc170/B.py
@@ -0,0 +1,10 @@
+x, y = map(int, input().split())
+
+j1 = y // 2 - x
+j2 = x*2 - y // 2
+
+if y % 2 == 0 and j1 >= 0 and j1 <= 25 and j2 >= 0 and j2 <= 50:
+    print('Yes')
+    exit()
+
+print('No')
