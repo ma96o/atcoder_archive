c4fc88e52e22a369488987c22742281a2ddec711
masaaki1915 <masaaki1915@gmail.com>
2021-04-08 13:58:21 +0900

    abc174 B

---
+++ src/abc174/B.py
@@ -0,0 +1,10 @@
+import math
+n, d = map(int, input().split())
+
+cnt = 0
+for i in range(n):
+    x, y = map(int, input().split())
+    if d >= math.sqrt(x**2 + y**2):
+        cnt += 1
+
+print(cnt)
