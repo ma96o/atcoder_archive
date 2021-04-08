35d5fa78c73e138e6a5d30dea4fdb2554ba69fe8
masaaki1915 <masaaki1915@gmail.com>
2021-04-08 13:18:01 +0900

    abc176 B

---
+++ src/abc176/B.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+if n % 9 == 0:
+    print('Yes')
+    exit()
+
+print('No')
+
+# n = input()
+# num = 0
+
+# for i in range(len(n)):
+#     num += int(n[i])
+
+# if num % 9 == 0:
+#     print('Yes')
+#     exit()
+
+# print('No')
