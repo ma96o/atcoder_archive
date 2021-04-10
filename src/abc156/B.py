c09e1df406888a121d3716762e5680e0abc60c23
masaaki1915 <masaaki1915@gmail.com>
2021-04-10 17:57:26 +0900

    abc156 B: add better answer

--- src/abc156/B.py
+++ src/abc156/B.py
@@ -9,3 +9,13 @@ def base_10_t0_n(x, n):
 
 NK = base_10_t0_n(N, K)
 print(len(NK))
+
+
+# print(int(math.log(N, K)+1))
+
+# S = ""
+# while N > 0:
+#     S = S + str(N % K)
+#     N = N // K
+
+# print(len(S))
