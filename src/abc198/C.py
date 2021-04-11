fd065f00e6d6829b6da712cb5255a530671c202e
masaaki1915 <masaaki1915@gmail.com>
2021-04-11 22:46:06 +0900

    abc198 C: add better answer

--- src/abc198/C.py
+++ src/abc198/C.py
@@ -4,3 +4,11 @@ xy = int(pow(x, 2) + pow(y, 2))**0.5
 
 ans = int(-(-xy//r)) if xy >= r else 2
 print(ans)
+
+d = math.sqrt(x*x + y*y)
+if d == r:
+    print(1)
+elif d <= r+r:
+    print(2)
+else:
+    print(math.ceil(d/r))
