c9680e6cd2ed75d12c05218c1d348bd65d0933f6
masaaki1915 <masaaki1915@gmail.com>
2021-04-11 10:01:58 +0900

    abc152 B: add better answer3

--- src/abc152/B.py
+++ src/abc152/B.py
@@ -4,6 +4,6 @@ print(str(a)*b if a <= b else str(b)*a)
 
 # print(str(min(a, b) * max(a, b)))
 
-sa = str(a)*b
-sb = str(b)*a
-print([sb, sa][a < b])
+# sa = str(a)*b
+# sb = str(b)*a
+# print([sb, sa][a < b])
