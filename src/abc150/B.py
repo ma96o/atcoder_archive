6d8bf388c9245db7e01e59accadfecf9796e03e8
masaaki1915 <masaaki1915@gmail.com>
2021-04-11 10:16:35 +0900

    abc150 B

---
+++ src/abc150/B.py
@@ -0,0 +1,13 @@
+n = int(input())
+s = input()
+
+print(s.count('ABC'))
+
+# cnt = 0
+# i = 0
+# while i < n-2:
+#     if s[i] + s[i+1] + s[i+2] == 'ABC':
+#         cnt += 1
+#     i += 1
+
+# print(cnt)
