d57f9ed440f82d77a2104525fbf97ddea13812c3
masaaki1915 <masaaki1915@gmail.com>
2021-04-11 11:38:18 +0900

    abc146 B: add better answer

--- src/abc146/B.py
+++ src/abc146/B.py
@@ -4,3 +4,9 @@ alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 for i in range(len(s)):
     print(alpha[alpha.index(s[i])+n-26], end='')
+
+# ans = ''
+# for s in S:
+#     ans += chr(ord('A') + (ord(s)-ord('A')+n) % 26)
+
+# print(ans)
