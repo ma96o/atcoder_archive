92643ecffaa8c7976b649f895c1fc329907ca754
masaaki1915 <masaaki1915@gmail.com>
2021-04-13 13:07:09 +0900

    abc141 B: add better answer

--- src/abc141/B.py
+++ src/abc141/B.py
@@ -12,3 +12,13 @@ for i in range(1, len(S)+1):
             exit()
 
 print("Yes")
+
+
+# for i in range(len(S)):
+#     if i % 2 == 0 and S[i] == "L":
+#         print("No")
+#         exit()
+#     if i % 2 == 1 and S[i] == "R":
+#         print("No")
+#         exit()
+# print("Yes")
