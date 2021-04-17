73687bee53eea6fb2befae4fc89bf9f12bfddde7
masaaki1915 <masaaki1915@gmail.com>
2021-04-17 15:09:50 +0900

    abc129 B

---
+++ src/abc129/B.py
@@ -0,0 +1,17 @@
+n = int(input())
+wlist = list(map(int, input().split()))
+
+s1 = 0
+s2 = sum(wlist)
+ans = s2
+
+for t in range(n):
+    s1 += wlist[t]
+    s2 -= wlist[t]
+    diff = abs(s1 - s2)
+    if ans >= diff:
+        ans = diff
+    else:
+        break
+
+print(ans)
