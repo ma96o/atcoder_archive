b323ad83eac53fcd070a2c1492ad19f7b4082e8b
masaaki1915 <masaaki1915@gmail.com>
2021-04-18 13:29:57 +0900

    abc125 B : add better answer

--- src/abc125/B.py
+++ src/abc125/B.py
@@ -2,10 +2,17 @@ n = int(input())
 vlist = list(map(int, input().split()))
 clist = list(map(int, input().split()))
 
+
 ans = 0
 for i in range(n):
     x = vlist[i]
     y = clist[i]
     ans += x-y if x-y > 0 else 0
+    # ans += max(x-y, 0)
+
 
 print(ans)
+
+
+# print(sum(v-c for v,c in zip(*[[*map(int,input().split())] for _ in range(2)]) if v>c))
+# print(sum(max(v-c, 0) for v, c in zip(vlist, clist)))
