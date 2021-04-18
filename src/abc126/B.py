25970a1c2073366a1485fc066a93e91fbd29b91c
masaaki1915 <masaaki1915@gmail.com>
2021-04-18 13:16:51 +0900

    abc126 B: add better answer

--- src/abc126/B.py
+++ src/abc126/B.py
@@ -6,6 +6,9 @@ behind = int(s[2:])
 fmm = 1 <= front and front <= 12
 bmm = behind >= 1 and behind <= 12
 
+# fmm = 1 <= front <= 12
+# fmm = 1 <= behind <= 12
+
 ans = "NA"
 
 if fmm and bmm:
@@ -16,3 +19,6 @@ elif fmm:
     ans = "MMYY"
 
 print(ans)
+
+
+# print(["NA","YYMM", "MMYY", "AMBIGUOUS"][2*(0<s//100<13)+(0<a%100<13)])
