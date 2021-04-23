fdf686d21f9532f1829875a871b5c064ca37a6d1
masaaki1915 <masaaki1915@gmail.com>
2021-04-23 12:03:10 +0900

    abc113 B

--- src/abc113/B.py
+++ src/abc113/B.py
@@ -1,9 +1,14 @@
 n = int(input())
 t, a = map(int, input().split())
-hlist = [map(int, input().split())]
-diffs = []
+hlist = list(map(int, input().split()))
+ans = 0
+temp = 10**5
 
 for i in range(n):
     h = hlist[i]
-    diff = abs(a-(t-h*0.006))
-    diffs.append(diff)
+    diff = abs((t-h*0.006)-a)
+    if diff < temp:
+        temp = diff
+        ans = i+1
+
+print(ans)
