74d73511ca1357e094faec72f66d0c0422bf1549
masaaki1915 <masaaki1915@gmail.com>
2021-05-07 16:57:37 +0900

    abc071 B: add better answer

--- src/abc071/B.py
+++ src/abc071/B.py
@@ -1,10 +1,17 @@
 s = input()
 
 alpha = 'abcdefghifklmnopqrstuvwxyz'
+# alpha = [chr(i) for i in range(97, 123)]
 
 for a in alpha:
     if not a in s:
         print(a)
         exit()
+
+# for i in range(97, 97+26):
+# for i in range(ord('a'), ord('z')+1):
+#     if chr(i) not in s:
+#         print(chr(i))
+#         exit()
 
 print('None')
