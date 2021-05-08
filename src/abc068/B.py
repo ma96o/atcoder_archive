eb15db6022a1750b1bfd842328376d2ca28eea1d
masaaki1915 <masaaki1915@gmail.com>
2021-05-08 14:52:30 +0900

    abc068 B: add better answer

--- src/abc068/B.py
+++ src/abc068/B.py
@@ -14,3 +14,16 @@ for i in range(2, n+1)[::2]:
 
 
 print(ans)
+
+
+# for a in range(7):
+#     if 2**a <= n and n < 2**(a+1):
+#         print(str(2**a))
+
+# ans = 1
+# while ans <= n/2:
+#     ans *= 2
+# print(ans)
+
+# n = bin(int(input()))
+# print(1 << len(n[2:])-1)
